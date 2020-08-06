4c2a8db284dd91b1aa3fb197f56c0ed03014d38b
Kyle <kjknudtson1989@gmail.com>
2020-08-06 16:13:57 -0500

    Adjusted configuration for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Vileka18Morty20!",
     database: "burgers_db"
-});
+}); 
+};
 
 
 connection.connect(function(err) {
